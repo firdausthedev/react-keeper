76c78a41e61c9a699019f83c5e08a2a3be379d7c
firdauscomel <bilbofirdaus@gmail.com>
2019-12-27 01:42:03 +0800

    Updates

---
+++ precache-manifest.a2501f2fd1da2f340104a16ea153ca55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77cda3154925e6e9e112681c8981acf3",
+    "url": "/react-keeper/index.html"
+  },
+  {
+    "revision": "6a74c3632075d0233139",
+    "url": "/react-keeper/static/js/2.d2d829c1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-keeper/static/js/2.d2d829c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "536eef3b8af5dd67fa77",
+    "url": "/react-keeper/static/js/main.183472ef.chunk.js"
+  },
+  {
+    "revision": "e7a76de44a8d3172c2f0",
+    "url": "/react-keeper/static/js/runtime-main.75701fd6.js"
+  }
+]);
